2e1241be80c96bde526b1c7b181fef6cb28d5c00
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-01-19 01:36:13 -0600

    Revert "feat: Add localization support"
    This reverts commit 899157f150deab3e1dfe8926ec3d0c97dbf5c593.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:todone/router.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,25 +9,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AdaptiveTheme(
-      light: ThemeData.light().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.cyan[700]!,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
-      ),
-      dark: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.cyan[700]!,
-        ),
-      ),
-      initial: AdaptiveThemeMode.system,
-      builder: (theme, darkTheme) => MaterialApp.router(
-        routerConfig: router,
-        title: "ToDone",
-        theme: theme,
-        darkTheme: darkTheme,
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
       ),
     );
   }
