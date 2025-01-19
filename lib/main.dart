899157f150deab3e1dfe8926ec3d0c97dbf5c593
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-01-18 21:13:19 -0600

    feat: Add localization support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:todone/router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return AdaptiveTheme(
+      light: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.cyan[700]!,
         ),
+      ),
+      dark: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.cyan[700]!,
+        ),
+      ),
+      initial: AdaptiveThemeMode.system,
+      builder: (theme, darkTheme) => MaterialApp.router(
+        routerConfig: router,
+        title: "ToDone",
+        theme: theme,
+        darkTheme: darkTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
       ),
     );
   }
