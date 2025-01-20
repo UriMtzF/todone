c49cf2dbbeadee33373a1035ab1b5f1d51ef09b5
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-01-19 22:26:39 -0600

    feat: Add dark and light themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,45 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:todone/i18n/strings.g.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(TranslationProvider(child: const MainApp()));
+  usePathUrlStrategy();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(TranslationProvider(child: MainApp(savedThemeMode: savedThemeMode)));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, this.savedThemeMode});
+  final AdaptiveThemeMode? savedThemeMode;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return AdaptiveTheme(
+      light: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan[700]!),
       ),
-      locale: TranslationProvider.of(context).flutterLocale,
-      supportedLocales: AppLocaleUtils.supportedLocales,
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      dark: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan[700]!),
+      ),
+      debugShowFloatingThemeButton: true,
+      initial: savedThemeMode ?? AdaptiveThemeMode.dark,
+      builder: (theme, darkTheme) {
+        return MaterialApp(
+          home: const Scaffold(
+            body: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+          theme: theme,
+          darkTheme: darkTheme,
+          locale: TranslationProvider.of(context).flutterLocale,
+          supportedLocales: AppLocaleUtils.supportedLocales,
+          localizationsDelegates: GlobalMaterialLocalizations.delegates,
+        );
+      },
     );
   }
 }
