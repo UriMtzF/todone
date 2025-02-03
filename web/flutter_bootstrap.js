9b568b5743ea390689aec9f8150f7bdeb6b6948f
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-02-03 00:35:08 -0600

    fix: Delete strong cache service
    A cache busting technique was added to improve the updated on web

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,8 @@
+{{flutter_js}}
+{{flutter_build_config}}
+_flutter.buildConfig.builds[0].mainJsPath += "?v=" + serviceWorkerVersion;
+_flutter.loader.load({
+    serviceWorkerSettings: {
+        serviceWorkerVersion: serviceWorkerVersion
+    }
+});
